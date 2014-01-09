d847216ab5f6e6cb029f3df77470000092bafdf6
Anders Einar Hilden <hildenae@gmail.com>
2014-01-09 13:30:52 +0100

    Add unix timestamp to messages

--- parse.py
+++ parse.py
@@ -3,6 +3,7 @@ import re # regexp
 import os # rename
 import json # dumps
 import datetime # current date
+from time import time as unix_timestamp # time.time()
 
 qsize = 20;
 chat_file = "chat.json"
@@ -31,9 +32,7 @@ def DTM(msg, time, match):
 def put(action,timestamp,nick,message):
 	if(len(storage) > qsize):
 		storage.pop(0)
-	#message = message.replace("<", "&lt;").replace(">", "&gt;")
-	#nick = nick.replace("<", "&lt;").replace(">", "&gt;")
-	storage.append({'action':action,'timestamp':timestamp,'nick':nick,'message':message})
+	storage.append({'action':action,'timestamp':timestamp,'nick':nick,'message':message,'unix_timestamp':unix_timestamp()})
 	writeJSON()
 
 def writeJSON():
